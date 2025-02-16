3d0aec89ae8146cc770285a2f0f0a0618efad4ce
Israel Dryer <israel.dryer@appforia.net>
2025-02-16 01:39:06 -0500

    set temporary button placement

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonRouterLink} from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonRouterLink,
+  IonIcon
+} from '@ionic/angular/standalone';
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonRouterLink, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonRouterLink, RouterLink, IonIcon],
 })
 export class HomePage {
   constructor() {}
