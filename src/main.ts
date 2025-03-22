03ff491515e4ba74478c750ac1a33fa083ceaeec
Israel Dryer <israel.dryer@appforia.net>
2025-03-21 23:48:36 -0400

    remove settings page headings

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ export const APP_VERSION = new InjectionToken<string>('APP_VERSION');
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {provide: APP_VERSION, useValue: '0.0.3'},
+    {provide: APP_VERSION, useValue: '0.0.4'},
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
     provideAnimationsAsync(),
